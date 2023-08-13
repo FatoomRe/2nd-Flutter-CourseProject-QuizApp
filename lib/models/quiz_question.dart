e4d61f0115d23fbad8a6a0b860f142a9544ce97d
Fatimah <Nyzak1@outlook.sa>
2023-08-13 21:38:33 +0300

    66. Adding a Data Model & Dummy Data

---
+++ lib/models/quiz_question.dart
@@ -0,0 +1,10 @@
+// this file will contain the questions blueprint
+
+class QuizQuestion {
+
+  const QuizQuestion(this.text, this.answers); //cosntructor
+
+  final String text;
+  final List<String> answers;
+}
+
