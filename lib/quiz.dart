f218b5927bd86c5e51366981bdbcaccfc8bb22f5
Fatimah <Nyzak1@outlook.sa>
2023-08-13 20:40:34 +0300

    62. Using Ternary Expressions & Comparison Operators

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -2,49 +2,42 @@ import 'package:flutter/material.dart';
 import 'package:secondcourseapp/questions_screen.dart';
 import 'package:secondcourseapp/start_screen.dart';
 
-
 class Quiz extends StatefulWidget {
   const Quiz({super.key});
 
   @override
-  State<Quiz> createState(){
+  State<Quiz> createState() {
     return _QuizState();
   }
 }
 
-
 class _QuizState extends State<Quiz> {
+  var activeScreen = 'start-screen';
 
-  Widget? activeScreen;// the ? tells dart that this variable can be a widget or null
-  @override
-  void initState(){
-    activeScreen = StartScreen(switchScreen);
-    super.initState();
-  }
-
-  void switchScreen(){
+  void switchScreen() {
     setState(() {
-      activeScreen = const QuestionsScreen();
+      activeScreen = 'questions-screen';
     });
   }
 
-  
   @override
   Widget build(context) {
     return MaterialApp(
-    debugShowCheckedModeBanner: false, //this will remove debug banner
-    home: Scaffold(
-      body: Container(
+      debugShowCheckedModeBanner: false, //this will remove debug banner
+      home: Scaffold(
+        body: Container(
           decoration: const BoxDecoration(
               gradient: LinearGradient(
-            colors: [Color(0xff673ab7),Color(0xffe91e63)],
+            colors: [Color(0xff673ab7), Color(0xffe91e63)],
             stops: [0, 1],
             begin: Alignment.bottomCenter,
             end: Alignment.topCenter,
           )),
-          child: activeScreen,
-          ),
-    ),
-  );
+          child: activeScreen == 'start-screen'
+              ? StartScreen(switchScreen)
+              : const QuestionsScreen(),
+        ),
+      ),
+    );
   }
 }
