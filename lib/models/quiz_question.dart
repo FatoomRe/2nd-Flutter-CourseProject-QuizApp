4e039b9d817ba6e6d7c43d0ff12589fe11bb2afd
Fatimah <Nyzak1@outlook.sa>
2023-08-14 22:44:20 +0300

    72. Mutating Values in Memory

--- lib/models/quiz_question.dart
+++ lib/models/quiz_question.dart
@@ -6,5 +6,11 @@ class QuizQuestion {
 
   final String text;
   final List<String> answers;
+
+  List<String> getAShuffledAnswer() {
+    final shuffledList = List.of(answers); // 1.create copy of answers
+    shuffledList.shuffle(); // 2.call shuffle method on the copy
+    return shuffledList; // 3.return the shuffled copy
+  }
 }
 
