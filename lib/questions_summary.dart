f455aff3b4322ba60b80560fffc997920955ad3f
Fatimah <Nyzak1@outlook.sa>
2023-08-15 15:47:06 +0300

    84. Combining Columns & Rows

--- lib/questions_summary.dart
+++ lib/questions_summary.dart
@@ -11,8 +11,18 @@ class QuestionsSummary extends StatelessWidget {
       children: summaryData.map((data) {
         return Row(
           children: [
-            Text(((data['question'] as int) + 1).toString()), // type casting
-          ],);
+            Text(((data['question_index'] as int) + 1)
+                .toString()), // type casting
+            Column(
+              children: [
+                Text(data['question'] as String),
+                const SizedBox(height: 5,),
+                Text(data['user_answer'] as String),
+                Text(data['correct_answer'] as String),
+              ],
+            ),
+          ],
+        );
       }).toList(),
     );
   }
