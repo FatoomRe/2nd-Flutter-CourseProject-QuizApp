649ee7f3ac7066b625ea7c272f83b43b73367693
Fatimah <Nyzak1@outlook.sa>
2023-08-16 09:56:03 +0300

    complate the assignment

--- lib/questions_summary.dart
+++ lib/questions_summary.dart
@@ -11,18 +11,18 @@ class QuestionsSummary extends StatelessWidget {
       height: 300,
       child: SingleChildScrollView(
         child: Column(
+          
           children: summaryData.map((data) {
             return Row(
               children: [
-                Text(((data['question_index'] as int) + 1)
-                    .toString()), // type casting
+                Text(((data['question_index'] as int) + 1).toString(), style: const TextStyle(fontWeight: FontWeight.bold,),), // type casting
                 Expanded(
                   child: Column(
                     children: [
-                      Text(data['question'] as String),
+                      Text(data['question'] as String, style: const TextStyle(fontWeight: FontWeight.bold, color: Colors.white)),
                       const SizedBox(height: 5,),
-                      Text(data['user_answer'] as String),
-                      Text(data['correct_answer'] as String),
+                      Text(data['user_answer'] as String , style: const TextStyle(fontWeight: FontWeight.bold, color: Color.fromARGB(255, 243, 229, 33))),
+                      Text(data['correct_answer'] as String, style: const TextStyle(fontWeight: FontWeight.bold,color: Color.fromARGB(255, 85, 179, 255))),
                     ],
                   ),
                 ),
